af5dbd9cb5e661e24c33747c1f55324f51681369
gigabelka <51454713+gigabelka@users.noreply.github.com>
2021-11-26 22:10:16 +0500

    запустил socket.io

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-const path = require('path');
 const express = require('express');
 const app = express();
 const http = require('http');
 const httpServer = http.createServer(app);
+const path = require('path');
+const { Server } = require("socket.io");
+const io = new Server(httpServer);
+
 const PORT = process.env.PORT || 3000;
+const fps = 1;
+let updtTime = 1000;
+let timer = null;
+let motion = false;
 
-// // array of connected websocket clients
-// let connectedClients = [];
+const autoUpdate = () => {
+    io.emit('image', 'data');
 
-// wsServer.on('connection', (ws, req) => {
-//     console.log('Connected');
-//     // add new connected client
-//     connectedClients.push(ws);
-//     // listen for messages from the streamer, the clients will not send anything so we don't need to filter
-//     ws.on('message', data => {
-//         // send the base64 encoded frame to each connected ws
-//         connectedClients.forEach((ws, i) => {
-//             if (ws.readyState === ws.OPEN) { // check if it is still connected
-//                 ws.send(data); // send
-//             } else { // if it's not connected remove from the array of connected ws
-//                 connectedClients.splice(i, 1);
-//             }
-//         });
+    clearTimeout(timer);
+    timer = setTimeout(autoUpdate, updtTime / fps);
+};
+autoUpdate();
+
+// io.on('connection', (socket) => {
+//     socket.on('image', data => {
+//         io.emit('image', data);
+//     });
+//     socket.on('motion', data => {
+//         io.emit('chat message', data);
 //     });
 // });
 
-// app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, '/index.html')));
+app.use('/', express.static(__dirname + '/static'));
 // app.get('/', (req, res) => {
 
 // });
-app.use('/', express.static(__dirname + '/static'));
-// app.get('/streamer', (req, res) => res.sendFile(path.resolve(__dirname, '/streamer.html')));
+
 httpServer.listen(PORT, () => console.log(`HTTP server listening at http://localhost:${PORT}`));
