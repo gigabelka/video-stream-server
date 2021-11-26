28016997f4f8c876d60c70b0b13de4cec338e231
gigabelka <51454713+gigabelka@users.noreply.github.com>
2021-11-26 23:45:40 +0500

    запустил стрим на статике.

--- index.js
+++ index.js
@@ -5,33 +5,35 @@ const httpServer = http.createServer(app);
 const path = require('path');
 const { Server } = require("socket.io");
 const io = new Server(httpServer);
+const mjpegDecoder = require('mjpeg-decoder');
 
 const PORT = process.env.PORT || 3000;
-const fps = 1;
+const streamURL = 'http://192.168.0.11/motion/live/1';
 let updtTime = 1000;
 let timer = null;
 let motion = false;
+let frame = null;
 
-const autoUpdate = () => {
-    io.emit('image', 'data');
+const getFrame = async function (interval) {
+    const decoder = new mjpegDecoder(streamURL, { interval: interval });
 
-    clearTimeout(timer);
-    timer = setTimeout(autoUpdate, updtTime / fps);
+    decoder.on('frame', (frm, seq) => {
+        frame = frm;
+        const image = frm.toString('base64');
+        io.emit('image', image);
+    });
+
+    decoder.on('abort', (reason, err) => {
+        console.log('decoder aborted for %s', reason, err);
+    });
+
+    decoder.start();
 };
-autoUpdate();
-
-// io.on('connection', (socket) => {
-//     socket.on('image', data => {
-//         io.emit('image', data);
-//     });
-//     socket.on('motion', data => {
-//         io.emit('chat message', data);
-//     });
-// });
 
 app.use('/', express.static(__dirname + '/static'));
-// app.get('/', (req, res) => {
+// app.get('/', (req, res) => {});
 
-// });
-
-httpServer.listen(PORT, () => console.log(`HTTP server listening at http://localhost:${PORT}`));
+httpServer.listen(PORT, () => {
+    console.log(`HTTP server listening at http://localhost:${PORT}`)
+    getFrame(updtTime);
+});
