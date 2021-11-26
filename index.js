6e84546cc47e20c08a3f41c29ee9b1f723f1ce4f
gigabelka <51454713+gigabelka@users.noreply.github.com>
2021-11-27 01:43:24 +0500

    добавил motion

--- index.js
+++ index.js
@@ -9,20 +9,34 @@ const mjpegDecoder = require('mjpeg-decoder');
 
 const PORT = 3000;
 const streamURL = 'http://192.168.0.11/motion/live/1';
-let updtTime = 1000;
+let sleepTime = 60000;
+let moveTime = 100;
 let motion = false;
 let frame = null;
-const decoder = new mjpegDecoder(streamURL, { interval: updtTime });
+let decoder = new mjpegDecoder(streamURL, { interval: sleepTime });
 
 const getFrame = async function () {
     decoder.on('frame', (frm, seq) => {
         frame = frm;
         const image = frm.toString('base64');
         io.emit('image', image);
+
+        if(motion){
+            io.emit('motion', true);
+        } else {
+            io.emit('motion', false);
+        }
     });
 
     decoder.on('abort', (reason, err) => {
-        console.log('decoder aborted for %s', reason, err);
+        if (motion){
+            decoder = new mjpegDecoder(streamURL, { interval: moveTime });
+            console.log('motion');
+        } else {
+            decoder = new mjpegDecoder(streamURL, { interval: sleepTime });
+            console.log('no motion');
+        }
+        getFrame();
     });
 
     decoder.start();
@@ -30,9 +44,19 @@ const getFrame = async function () {
 };
 
 app.use('/', express.static(__dirname + '/static'));
-// app.get('/', (req, res) => {});
+app.get('/motion', (req, res) => {
+    if(req.query.move == 'yes'){
+        motion = true;
+        decoder.stop();
+    } else {
+        motion = false;
+        decoder.stop();
+    }
+});
 
 httpServer.listen(PORT, () => {
     console.log(`HTTP server listening at http://localhost:${PORT}`)
     getFrame();
 });
+
+// http://localhost:3000/motion?move=yes(no)
